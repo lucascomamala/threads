c1ef87f143d1dbbe995d15a1d7ded03e367b7ef5
Lucas <lucascomamala@gmail.com>
2023-08-18 16:51:05 -0600

    Create thread page

--- components/index.ts
+++ components/index.ts
@@ -4,5 +4,6 @@ import LeftSidebar from "./shared/LeftSidebar";
 import RightSidebar from "./shared/RightSidebar";
 
 import AccountProfile from "./forms/AccountProfile";
+import PostThread from "./forms/PostThread";
 
-export { Topbar, Bottombar, LeftSidebar, RightSidebar, AccountProfile };
+export { Topbar, Bottombar, LeftSidebar, RightSidebar, AccountProfile, PostThread };
