00927b47fd6e98a866b0ecbb87fb9f8afea00789
Lucas <lucascomamala@gmail.com>
2023-08-19 15:56:16 -0600

    Profile header

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from 'next/navigation'
 import { fetchUser } from "@/lib/actions/user.actions"
 import { PostThread } from "@/components/index"
 
-async function Page() {
+const Page = async () => {
   const user = await currentUser()
   if (!user) return null
   
