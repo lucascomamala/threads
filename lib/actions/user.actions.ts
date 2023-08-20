f80ea1f85d04ff21afd6f7d08237b35053157e28
Lucas <lucascomamala@gmail.com>
2023-08-19 20:49:30 -0600

    fetch users action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache"
 import User from "../models/user.model"
 import { connectToDB } from "../mongoose"
 import Thread from "../models/thread.model"
+import { FilterQuery, SortOrder } from "mongoose"
 
 interface Params {
   userId: string
@@ -83,5 +84,54 @@ export async function fetchUserPosts(userId: string) {
       })
   } catch (err: any) {
     throw new Error(`Failed to fetch user posts: ${err.message}`)
+  }
+}
+
+export async function fetchUsers({
+  userId,
+  searchString = '',
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = 'desc',
+} : {
+  userId: string
+  searchString?: string
+  pageNumber?: number
+  pageSize?: number
+  sortBy?: SortOrder
+}) {
+  try {
+    connectToDB();
+
+    const skip = (pageNumber - 1) * pageSize
+
+    const regex = new RegExp(searchString, 'i')
+
+    const query: FilterQuery<typeof User> = {
+      id: { $ne: userId },
+    }
+
+    if(searchString.trim() !== '') {
+      query.$or = [
+        { username: regex },
+        { name: regex },
+      ]
+    }
+
+    const sortOptions = { createdAt: sortBy }
+    const usersQuery = User
+      .find(query)
+      .sort(sortOptions)
+      .skip(skip)
+      .limit(pageSize)
+
+    const totalUsersCount = await User.countDocuments(query)
+    const users = await usersQuery.exec()
+
+    const isNext = totalUsersCount > skip + users.length
+
+    return { users, isNext }
+  } catch (error: any) {
+    throw new Error(`Failed to fetch users: ${error.message}`)
   }
 }
