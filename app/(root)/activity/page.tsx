6d74921b3874e4ee5a1ba9c6e1dccc6b46d501c4
Lucas <lucascomamala@gmail.com>
2023-08-19 21:15:03 -0600

    get activity action

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,4 +1,17 @@
+import { currentUser } from "@clerk/nextjs"
+import { redirect } from 'next/navigation'
+
+import { fetchUser } from "@/lib/actions/user.actions"
+
 const Page = async () => {
+  const user = await currentUser()
+  if (!user) return null
+
+  const userInfo = await fetchUser(user.id)
+  if (!userInfo?.onboarded) redirect('/onboarding')
+
+  // get activity
+
   return (
     <section>
       <h1>Activity</h1>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -135,3 +135,34 @@ export async function fetchUsers({
     throw new Error(`Failed to fetch users: ${error.message}`)
   }
 }
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    //find all threads created by the user
+    const threads = await Thread
+      .find({ author: userId })
+
+    // find all the child thread ids (replies) from the children field
+    const childThreadIds = threads.reduce((acc, thread) => {
+      return acc.concat(thread.children)
+    })
+
+    const replies = await Thread
+      .find({ 
+        _id: { $in: childThreadIds },
+        author: { $ne: userId },
+      })
+      .populate({
+        path: 'author',
+        model: User,
+        select: 'name image _id'
+      })
+
+    return replies
+    
+  } catch (error: any) {
+    throw new Error(`Failed to fetch activity: ${error.message}`)
+  }
+}
